53d86866d6b63f7a62c11078e04e8f7f82879b1b
Andrey Salnikov <kvanta13@yandex.ru>
2022-01-02 02:33:05 +0300

    Добавьте файлы проекта.

---
+++ Abstract/Dag.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Dag:Animal
+    {
+        string name;
+        public override string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public Dag(string name)
+            : base(name)
+        {
+        }
+        public override void Say()
+        {
+            Console.WriteLine("ГАВ");
+        }
+    }
+}
---
+++ Abstract/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Имя кошки");
+            string nameCat = Console.ReadLine();
+            Console.WriteLine("Имя собаки");
+            string nameDog = Console.ReadLine();
+            Dag dag = new Dag(nameDog);
+            Cat cat = new Cat(nameCat);
+            Console.WriteLine("Инфо кошка");
+            cat.ShowInfo();
+            Console.WriteLine("Инфо собака");
+            dag.ShowInfo();
+            Console.ReadKey();
+        }
+    }
+}
